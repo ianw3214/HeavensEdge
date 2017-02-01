d70fe4cc68b6d9cb4e75836b22655dda7e1b0cdd
Ian <ianw3214@gmail.com>
2017-01-31 22:33:29 -0500

    Add devlog

--- shell.bat
+++ shell.bat
@@ -1,7 +1,6 @@
 @echo off
 doskey a = atom $*
 doskey ls = dir $*
-doskey jc = javac $*
 doskey tx = pdflatex $*
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall"
 cd "C:\Users\ianw3214\workspace\HeavensEdge"
